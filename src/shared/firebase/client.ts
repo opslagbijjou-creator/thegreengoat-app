c5d2a54c709be6fb5664b60b82ecc6ad6cea3725
hamza <opslagbijjou@gmail.com>
2025-12-17 19:24:59 +0100

    d

--- src/shared/firebase/client.ts
+++ src/shared/firebase/client.ts
@@ -1,6 +1,14 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FB_API_KEY,
   authDomain: import.meta.env.VITE_FB_AUTH_DOMAIN,
   projectId: import.meta.env.VITE_FB_PROJECT_ID,
   appId: import.meta.env.VITE_FB_APP_ID
 };
+
+const app = initializeApp(firebaseConfig);
+
+// ✅ DIT is wat je nu mist:
+export const auth = getAuth(app);
